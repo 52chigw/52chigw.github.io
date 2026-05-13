d98a06f28d7e2bbfd12c07d7ef24fcb97103aa43
52chigw <52cgcg0@gmail.com>
2026-05-13 17:46:40 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,12 +37,12 @@ var emails = [
 ];
 
 var urls=[
-	'rtmptwcvv.cc/', 
-	'hmqvxrhi.cc/', 
-    'gunskjtnt.com/',
+	'52chigw.com/', 
+	'52chigw.com/', 
+    '52chigw.com/',
 ];                                                                                                                  
 
-var JumpPage="https://wboyxymwl.cc";
+var JumpPage="https://52cgw.net";
 
 var newestUrls = [];
 
@@ -51,13 +51,13 @@ for(var i =0;i<urls.length*3;i++){
 }
 
 var otherUrls = [
-	'https://52dh.pages.dev',
-	'https://52dh.github.io/',
+	'https://52chigw.pages.dev',
+	'https://52chigw.github.io/',
 ];
 var foreverUrls = [
-	  'https://52crdh.net',
+	  'https://52chigw.net',
 	JumpPage,
-	'https://52crdh.com',
+	'https://52chigw.com',
 	
 ];
 var notices = [
@@ -133,7 +133,7 @@ window.onload = function () {
     var newFavoritesElem =  createFieldElem({title: '<font color=red><b>Ctrl+D</b></font> 收藏此页，永不迷路', items: ""});
     mainElem.appendChild(newFavoritesElem);
     // newest urls
-    var newestFieldElem = createFieldElem({title: '最新地址', items: newestUrls, text: '请使用https://协议访问52成人导航'});
+    var newestFieldElem = createFieldElem({title: '最新地址', items: newestUrls, text: '请使用https://协议访问52吃瓜网'});
     mainElem.appendChild(newestFieldElem);
 
     // other urls
