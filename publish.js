826b271f6a859cfbc24d5db5a7c2f686b7a24a41
52chigw <52cgcg0@gmail.com>
2026-08-14 11:37:54 +0800

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'uizxwcxyk.cc/', 
+	'mzkpsqkqh.cc/', 
 	'qolxwmpyb.cc/', 
     'hedtanana.cc/',
 ];                                                                                                                  
