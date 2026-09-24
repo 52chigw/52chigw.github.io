0cfe25ac46f44ec409aea8e7b9a410c047c0d76d
52chigw <52cgcg0@gmail.com>
2026-09-24 10:35:41 +0800

    Update publish.js

--- publish.js
+++ publish.js
@@ -38,7 +38,7 @@ var emails = [
 
 var urls=[
 	'svehwdte.com/', 
-	'nnmsuvwn.com/', 
+	'pwiuqveg.cc/', 
     'sswxwiyl.cc/',
 ];                                                                                                                  
 
